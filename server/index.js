36e3bea1fd11fd9355f8559a363fcb0fa2400505
Pritam <pri10145@gmail.com>
2024-07-16 16:13:55 +0530

    basic structure of server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import userRouter from './routes/user.routes.js'
+
+dotenv.config()
+
+mongoose.connect(process.env.MONOGODB_URL).then(() => {
+    console.log("Connected to MongoDB")
+}).catch(() => {
+    console.log("Failed to connect to MongoDB")
+})
 
 const app = express()
+
+app.use('/api/user', userRouter)
 
 app.listen(3000, () => {
     console.log("Server listening on port 3000")
