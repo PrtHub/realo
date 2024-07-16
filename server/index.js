bd93a1de478bdc2f926e46ff9415dcd318f07ea0
Pritam <pri10145@gmail.com>
2024-07-16 19:03:59 +0530

    signup route and error handling

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 import userRouter from './routes/user.routes.js'
+import authRouter from './routes/auth.routes.js'
 
 dotenv.config()
 
@@ -12,9 +13,21 @@ mongoose.connect(process.env.MONOGODB_URL).then(() => {
 })
 
 const app = express()
-
-app.use('/api/user', userRouter)
+app.use(express.json())
 
 app.listen(3000, () => {
     console.log("Server listening on port 3000")
+})
+
+app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || "Internal Server Error"
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
 })
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.model(
+const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
