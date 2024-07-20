432fbb35c0e0345d3fd375b494d395695c01f187
Pritam <pri10145@gmail.com>
2024-07-20 12:53:53 +0530

    get all properties api

--- server/controllers/property.controller.js
+++ server/controllers/property.controller.js
@@ -64,3 +64,45 @@ export const getTheProperty = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getAllProperties = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+
+    const sort = req.query.sort || "createdAt";
+
+    const order = req.query.order || "desc";
+
+    const properties = await Property.find({
+      name: { $regex: searchTerm, $options: "i" },
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    res.status(200).json(properties);
+  } catch (error) {
+    next(error);
+  }
+};
--- server/routes/property.routes.js
+++ server/routes/property.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createProperty, deleteProperty, getTheProperty, updateProperty } from '../controllers/property.controller.js';
+import { createProperty, deleteProperty, getAllProperties, getTheProperty, updateProperty } from '../controllers/property.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post('/create', verifyToken, createProperty)
 router.delete('/delete/:id', verifyToken, deleteProperty)
 router.post('/update/:id', verifyToken, updateProperty)
 router.get('/get-property/:id', getTheProperty)
+router.get('/all-properties', getAllProperties)
 
 export default router
